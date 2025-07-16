25e7aba7857b6435e0233e4ea3e111a644c478aa
Minh161124 <donhatminh161124@gmail.com>
2025-07-16 22:12:57 +0700

    16072025

--- lesson_one/main.js
+++ lesson_one/main.js
@@ -1,1 +1,17 @@
-alert('Hi javascript basic')
+// var fullName = 'Do Nhat Minh';
+// var i=5;
+// var age = i+2; 
+// alert('你好'+fullName+' '+age+'岁'+'，环境来到这里');
+// alert: thong bao
+// confirm: thong bao xac nhan
+// confirm('Xac nhan ban khong phai nguoi may!')
+// prompt: xac nhan + textbox
+// prompt('Nhap tuoi de tiep')
+// settimeout: truyen vao 1 funtion va truyen vao 1 time va chi chay 1 lan duy nhat
+// setTimeout(function() { alert('Thongbao')}, 1000)
+// setInterval: truyen vao 1 funtion va truyen vao 1 time va chay theo chu ki moiligiay da dat ra. 1000mls=1s
+// setInterval(function() { alert('Thongbao')}, 6000)
+var i =2;
+var a = i++;
+var b = a++;
+alert('c='+a+b);
